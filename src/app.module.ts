54d9fa30d6986016051c8eb67740c3929c964e12
D.Agafonova <d.agafonova@x5.ru>
2021-02-19 22:57:47 +0300

    Add work with sqlite, user entity and repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: `${__dirname}/db/mydb.sqlite`,
+      entities: [ User ],
+      logging: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
