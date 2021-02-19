02e944388963e0cc11497731f90b29238986835e
D.Agafonova <d.agafonova@x5.ru>
2021-02-20 01:10:44 +0300

    Add session work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body, Get, Session } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDTO } from './users/user.dto';
+import { UserGuard } from './users/user.guard';
 import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
   constructor(private usersService: UsersService) {}
   @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  @Get('auth/login')
+  async login(@Session() session: Record<string, any>, @Request() req) {
+    session.user = req.user;
+    return session.user;
   }
 
   @Post('user')
   async createUser(@Body() user: CreateUserDTO) {
     return this.usersService.createOne(user);
+  }
+
+  @Get('test')
+  @UseGuards(UserGuard)
+  test(@Session() session: Record<string, any>) {
+    return session.user;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { AuthFilter } from './auth/auth.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+    }),
+  );
+  app.useGlobalFilters(new AuthFilter());
   await app.listen(3000);
 }
 bootstrap();
